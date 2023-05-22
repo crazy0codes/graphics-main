c001e4d128fae75bfdb715b96e16c2e20a33a58e
crazy0codes <madhanpanja@gmail.com>
2023-05-22 17:15:25 +0530

    Uploaded files

---
+++ graphics-main/index.js
@@ -0,0 +1,15 @@
+let bg = ["https://c4.wallpaperflare.com/wallpaper/295/163/719/anime-anime-boys-picture-in-picture-kimetsu-no-yaiba-kamado-tanjir%C5%8D-hd-wallpaper-preview.jpg",
+     "https://c4.wallpaperflare.com/wallpaper/275/883/590/spiderman-ps4-spiderman-ps4-games-hd-wallpaper-preview.jpg"
+     ]
+
+let bg_for = document.querySelector('.subheading')
+// setTimeout(() => {
+//     bg_for.style.backgroundImage = bg[1]
+// },1000)
+
+document.addEventListener('DOMContentLoaded', function() {
+     document.querySelector('#welcome').style.backgroundColor = "black"
+     document.querySelector('#welcome').style.transitionDuration = "2s"
+     document.querySelector('#welcome').style.color = "white"
+});
+
