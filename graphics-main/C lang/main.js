8b9f40b72d0a3d306c42362cbf2c5969f3eb76c5
crazy0codes <madhanpanja@gmail.com>
2023-06-03 20:00:39 +0530

    copy btn

--- graphics-main/C lang/main.js
+++ graphics-main/C lang/main.js
@@ -1,1 +1,29 @@
 hljs.highlightAll()
+
+let copybtn = document.querySelector('.copy');
+const elements = document.querySelectorAll('.container');
+
+elements.forEach(element => {
+  const copyButton = document.createElement("button");
+  copyButton.className = "copy";
+  copyButton.innerText = "copy";
+  element.querySelector("pre").appendChild(copyButton);
+
+  copyButton.addEventListener("click", copyCode);
+});
+
+function copyCode(event) {
+  const codeElement = event.target.parentNode.querySelector("code");
+  const codeText = codeElement.innerText;
+
+  navigator.clipboard.writeText(codeText)
+    .then(() => {
+      this.innerText = "copied";
+      setTimeout(() => {
+        this.innerText = "copy";
+      }, 2000);
+    })
+    .catch((error) => {
+      console.error("Failed to copy code: ", error);
+    });
+}
