30183e45e5ccfc80b61221461973f5c24333e35b
crazy0codes <madhanpanja@gmail.com>
2023-06-03 15:48:06 +0530

    update

--- graphics-main/C lang/main.js
+++ graphics-main/C lang/main.js
@@ -1,1 +1,8 @@
 hljs.highlightAll()
+
+const container = document.querySelectorAll('.container')
+
+container.forEach(element => {
+    let innerMostElement = element.children[1]
+    console.log(innerMostElement)
+});
